724dee1c23fd38471fd3dc59ac6bb74f778009ec
David Moreno Magin <davidmorenomagin@icloud.com>
2018-07-25 23:33:27 +0200

    add, in develop class, TextPlainFile

--- dataaccess/textplain/TextPlainFile.cs
+++ dataaccess/textplain/TextPlainFile.cs
@@ -6,10 +6,9 @@ using System.Threading.Tasks;
 
 namespace es.dmoreno.utils.dataaccess.textplain
 {
-    public class TextPlainFile
+    public class TextPlainFile : IDisposable
     {
         private string _filename;
-        private string _content;
         private FileStream _fs;
 
         public TextPlainFile(string filename)
@@ -75,5 +74,46 @@ namespace es.dmoreno.utils.dataaccess.textplain
                 await sw.WriteAsync(text);
             }
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // Para detectar llamadas redundantes
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: elimine el estado administrado (objetos administrados).
+                    if (this._fs != null)
+                    {
+                        this._fs.Dispose();
+                        this._fs = null;
+                    }
+                }
+
+                // TODO: libere los recursos no administrados (objetos no administrados) y reemplace el siguiente finalizador.
+                // TODO: configure los campos grandes en nulos.
+                this._filename = null;
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: reemplace un finalizador solo si el anterior Dispose(bool disposing) tiene código para liberar los recursos no administrados.
+        // ~TextPlainFile() {
+        //   // No cambie este código. Coloque el código de limpieza en el anterior Dispose(colocación de bool).
+        //   Dispose(false);
+        // }
+
+        // Este código se agrega para implementar correctamente el patrón descartable.
+        public void Dispose()
+        {
+            // No cambie este código. Coloque el código de limpieza en el anterior Dispose(colocación de bool).
+            Dispose(true);
+            // TODO: quite la marca de comentario de la siguiente línea si el finalizador se ha reemplazado antes.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
